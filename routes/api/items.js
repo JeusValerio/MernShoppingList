17089f4d4be1cda7e079955a53945a3058688e0e
JeusValerio <valerio.234180@caloocan.sti.edu.ph>
2024-03-06 12:18:18 +0800

    init

--- routes/api/items.js
+++ routes/api/items.js
@@ -8,5 +8,22 @@ const Item = require('../../models/Item');
 //@desc Get All Items
 //@access Public
 
+router.get('/', (req,res) => {
+        Item.find()
+        .sort ({date: -1 }) //descending
+        .then(items => res.json(items))
+}); //represents actual endpoint using router
+
+//@route POST api/items //Post Request
+//@desc Add a POST
+//@access Public
+
+router.post('/', (req,res) => { //making a post request
+    //construct an object to insert into database
+    const newItem = new Item({
+        name: req.body.name
+    });
+});
+
 
 module.exports = router;
