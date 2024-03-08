397ab929c7b62af1e78dd79e8105069431b9a037
Jeus Valerio <valerio.234180@caloocan.sti.edu.ph>
2024-03-08 14:39:59 +0800

    create component AppNavbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+    Collapse, //responsive menu
+    Navbar,
+    NavbarToggle,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container //bootstrap container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    
+}
