60f0c88e7a608452ddece72ac29d33d366b54d1a
Jeus Valerio <valerio.234180@caloocan.sti.edu.ph>
2024-03-12 09:43:45 +0800

    Added Remove Button

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,36 +1,77 @@
-import React, {Component} from 'react';
-import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
-import {CSSTransition, TransitionGroup } from 'react-transition-group';
-import {v4 as uuidv4} from 'uuid';
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { v4 as uuidv4 } from 'uuid';
 
 class ShoppingList extends Component {
     state = {
         items: [
-            {id: uuidv4(), name: 'Eggs'},
-            {id: uuidv4(), name: 'Milk'},
-            {id: uuidv4(), name: 'Water'},
-            {id: uuidv4(), name: 'Vegetables'},
-        ]
+            { id: uuidv4(), name: 'Eggs' },
+            { id: uuidv4(), name: 'Milk' },
+            { id: uuidv4(), name: 'Water' },
+            { id: uuidv4(), name: 'Vegetables' },
+        ],
+        newItemName: '' // New state to hold the value of the new item input
     }
 
-    render(){
-        const { items } = this.state;
-        return(
+    handleChange = (e) => {
+        this.setState({ newItemName: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { newItemName } = this.state;
+        if (newItemName.trim()) {
+            this.setState(state => ({
+                items: [...state.items, { id: uuidv4(), name: newItemName }],
+                newItemName: '' // Clear input after adding item
+            }));
+        }
+    }
+
+    render() {
+        const { items, newItemName } = this.state;
+        return (
             <Container>
-                <Button 
-                color = "dark" 
-                style = {{marginBottom: '2rem' }}
-                onClick={() =>{
-                    const name = prompt('Enter your Item');
-                    if(name){
-                        this.setState(state => ({
-                            items: [...state.items, { id: uuidv4(), name }]
-                        }));
-                    }
-                }}
-                >
-                    Add Item
-                </Button>
+                <Form onSubmit={this.handleSubmit} style={{ marginBottom: '2rem' }}>
+                    <FormGroup>
+                        <Label for="itemName"><b>Add Item</b></Label>
+                        <div className="d-flex">
+                            <Input
+                                type="text"
+                                name="itemName"
+                                id="itemName"
+                                placeholder="Enter item name"
+                                value={newItemName}
+                                onChange={this.handleChange}
+                                style={{ marginRight: '1rem' }}
+                            />
+                            <Button color="dark" type="submit">Add</Button>
+                        </div>
+                    </FormGroup>
+                </Form>
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button
+                                    className="remove-btn"
+                                    color="danger"
+                                    size= "sm"
+                                    style={{ marginRight: '0.5rem' }}
+                                    onClick = {() => {
+                                        this.setState(state => ({
+                                            items: state.items.filter(item => item.id !== id)
+                                        }))
+                                    }}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
         );
     }
